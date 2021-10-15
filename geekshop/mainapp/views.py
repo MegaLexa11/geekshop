daa182c637448433e41f8db24daa2ae6671e02dc
MegaLexa <kukh99@mail.ru>
2021-10-15 17:17:09 +0500

    Homewrok 2 done
    Some work with templates, urls and views was made

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,10 +1,73 @@
 from django.shortcuts import render
 
+
 def main(request):
     return render(request, 'mainapp/index.html')
 
-def products(request):
-    return render(request, 'mainapp/products.html')
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+links_menu = [
+        {
+            'url': 'products',
+            'title': 'все',
+        },
+        {
+            'url': 'products_home',
+            'title': 'дом',
+        },
+        {
+            'url': 'products_office',
+            'title': 'офис',
+        },
+        {
+            'url': 'products_modern',
+            'title': 'модерн',
+        },
+        {
+            'url': 'products_classic',
+            'title': 'классика',
+        },
+    ]
+
+
+def products(request):
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_home(request):
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_office(request):
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_modern(request):
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+
+def products_classic(request):
+
+    context = {
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
