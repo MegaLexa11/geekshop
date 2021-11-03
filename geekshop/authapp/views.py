c76b59f45ae3961632e0aede141eef183dffee94
MegaLexa <kukh99@mail.ru>
2021-11-03 19:38:24 +0500

    Homework_6 done

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -8,6 +8,8 @@ from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditF
 def login(request):
     title = 'Вход'
 
+    next_param = request.GET.get('next', '')
+
     login_form = ShopUserLoginForm(data=request.POST)
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST.get('username')
@@ -16,11 +18,14 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
+            if 'next' in request.POST.keys():
+                return HttpResponseRedirect(request.POST['next'])
             return HttpResponseRedirect(reverse('index'))
 
     context = {
         'title': title,
-        'login_form': login_form
+        'login_form': login_form,
+        'next': next_param,
     }
     return render(request, 'authapp/login.html', context)
 
